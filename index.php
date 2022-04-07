0ede25706bc22b8a589a7d418eb73fc1cf21b522
Jaspal Khanuja <38846757+JaspalK2@users.noreply.github.com>
2022-04-07 01:37:08 -0700

    Added html/css components along with some documentation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <?php echo '<p>Hello World! Here are all my links Jaspal Khanuja Period 1</p>'; ?>
+  </head>
+   <ul>
+     <li><a href="index.html">index.html</a></li>
+     <li><a href="Khanuja_AboutMe.html">AboutMe.html</li>
+     <li><a href="Khanuja_ContactMe.html">ContactMe.html</a></li>
+     <li><a href="Khanuja_Projects.html">Projects.html</a></li>
+     <li><a href="Khanuja_Resume.html">Resume.html</a></li>
+   </ul>
+   
